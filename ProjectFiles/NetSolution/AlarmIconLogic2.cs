de04c8f95bccbd379c781d338157b865d418ecb7
KphungFROMM <kphung@fromm.com>
2024-05-23 08:45:50 -0400

    Pushing Project to Remote

---
+++ ProjectFiles/NetSolution/AlarmIconLogic2.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.System;
+using FTOptix.Alarm;
+#endregion
+
+public class AlarmIconLogic2 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
